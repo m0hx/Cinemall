67774eca347c22e5bc7815204f78c8da818dc33e
M0HX <M0HX@users.noreply.github.com>
2026-04-19 08:31:07 +0300

    - implement Movie Details Route/Page, movie showtimes list - select showtime redirects to /showtimes to select seats. >next

--- cinemall-web/src/App.tsx
+++ cinemall-web/src/App.tsx
@@ -4,6 +4,8 @@ import { HomePage } from './pages/HomePage.tsx'
 import { AboutPage } from './pages/AboutPage.tsx'
 import { ContactPage } from './pages/ContactPage.tsx'
 import { MoviesPage } from './pages/MoviesPage.tsx'
+import { MovieDetailPage } from './pages/MovieDetailPage.tsx'
+import { ShowtimePlaceholderPage } from './pages/ShowtimePlaceholderPage.tsx'
 import { SignInPage } from './pages/SignInPage.tsx'
 import { SignUpPage } from './pages/SignUpPage.tsx'
 
@@ -15,6 +17,8 @@ export default function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailPage />} />
+        <Route path="/showtimes/:showtimeId" element={<ShowtimePlaceholderPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Route>
